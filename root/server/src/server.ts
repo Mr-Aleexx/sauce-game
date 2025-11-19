d2b6deb3e001701dabd30a57033317fd17fa232c
Alex Hazet <alex.hazet@gmail.com>
2025-11-19 18:21:31 +0100

    DEL/Ts code and tried to make a js server

--- root/server/src/server.ts
+++
@@ -1,8 +0,0 @@
-import { createServer } from "http";
-import { handler } from "./handler"
-
-const port = 3000;
-
-const server = createServer(handler);
-
-server.listen(port, () => console.log(`Server listening on port ${port}`));
